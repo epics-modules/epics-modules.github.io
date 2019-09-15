2b24258d91f8dcfff8f66e20b20dda3aec3b8441
Mark Rivers <rivers@cars.uchicago.edu>
2019-09-15 11:46:20 -0500

    New version

--- master/html/_static/documentation_options.js
+++ master/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'c6ea65b',
+    VERSION: '8d7813b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
