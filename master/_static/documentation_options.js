d8cb7a5042c5e3823758315e77fce423f1bff5bd
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-09-15 21:49:19 +0000

    Update docs after building Travis build 18 of epics-modules/docs
    The docs were built from the branch 'master' against the commit
    b1bfa153377ced1b2b092fead740e35813d112f3.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/epics-modules/docs/jobs/585319210.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --key-path github_deploy_key_epics_modules_epics_modules_github_io.enc --build-tags --built-docs _build/html --deploy-repo epics-modules/epics-modules.github.io master

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'dcb7071',
+    VERSION: 'b1bfa15',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
